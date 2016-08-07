4e414b22dd1fcfee56c654dd3d301e3f39465c20
Felix Engelmann <xilef@posteo.net>
2016-08-07 12:54:01 +0100

    update to inter-act

---
+++ inter-act.r
@@ -0,0 +1,331 @@
+library(tidyr)
+library(dplyr)
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+## Global parameters
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+reset_params <- function(){
+  lf        <<- 0.15       # latency factor
+  le        <<- 1       # latency exponent
+  rth 	    <<- -1.5    # retrieval threshold
+  bll       <<- 0.5     # decay parameter
+  ans       <<- 0.2    # activation noise
+  mas       <<- 1       # maximum associative strength 
+  mp        <<- 1       # mismatch penalty
+  ga        <<- 1       # goal source activation
+  rand_time <<- 3       # latency variability
+  blc       <<- 0       # base-level activation
+  ##
+  lp        <<- 1     # default time since last presentation (msec)
+  ldp       <<- 1       # last distractor presentation (msec)
+  ## Distractor control:
+  ndistr    <<- 1				# number of distractors
+  dbl       <<- 0       # distractor base-level
+  ##
+  ## Cue weighting, cue confusion and prominence:
+  # cueweights <<- c(1,1) # vector of cue weightings
+  cueweighting <<- 1     # Strength of structural cue as ratio str/sem
+  normalizeWeights <<- TRUE 
+  qcf 		  <<- 1 			# match quality correction factor
+  qco 		  <<- -2*rth 			# match quality correction offset
+  psc       <<- 1       # prominence scaling constant C1
+  pic       <<- 0       # prominence scaling constant C2
+  tprom     <<- 0       # target prominence 
+  dprom     <<- 0       # distractor prominence
+  # cl 				<<- 0 			# cue confusion level (0-100)
+  cuesim    <<- -1    # cue-feature similarity [-1..0]
+  #
+  # Fitted meta-parameters
+  medprom <<- 0
+  highprom <<- 2
+  distant <<- 1.3
+  recent <<- 0.7
+  depp <<- 0
+  methp <<- 0
+  #
+  VERBOSE <<- TRUE
+}
+reset_params()
+
+cuesim2cl <- function(x=cuesim){
+	cl <<- (x+1)*100
+	cl
+}
+
+cl2cuesim <- function(x=cl){
+	cuesim <<- x/100-1
+	cuesim
+}
+
+strWeight <- function(ratio=cueweighting, normalize=normalizeWeights){
+  ifelse(normalize, ratio/(ratio+1)*2, ratio)
+}
+semWeight <- function(ratio=cueweighting, normalize=normalizeWeights){
+  ifelse(normalize, 1/(ratio+1)*2, 1)
+}
+
+idnames <- c("Set","Iteration","Condition","Target","Distractor")
+actrnames <- c("weights","bl1","bl2","times1","times2","noise1","noise2","blact1","blact2","act1","act2","activation","latency","retrieved","acc","miss","fail")
+paramnames <- c("lf","le","rth","bll","ans","mas","mp","ga","rand_time","lp","blc","ldp","dbl","ndistr","cueweighting","psc","pic","qcf", "qco" ,"cuesim","tprom","dprom","distant","recent","medprom","highprom", "methp", "depp")
+
+set_prameters <- function(){
+	parameters <<- list(lf,le,rth,bll,ans,mas,mp,ga,rand_time,lp,blc,ldp,dbl,ndistr,cueweighting,psc,pic,qcf, qco,cuesim,tprom,dprom,distant,recent,medprom,highprom, methp, depp)
+	names(parameters) <<- paramnames
+}
+set_prameters()
+
+
+# compute_int_means <- function(results){
+# 	## Extract interference effect
+# 	int <- select(filter(results, Distractor=="Match"), -Condition, -Distractor)
+# 	dim(int)
+# 	int$int <- filter(results, Distractor=="Match")$latency - filter(results, Distractor=="Mismatch")$latency
+# 	#
+# 	## Compute means
+# 	simMeans <- group_by(int, Set, Target, lf, ans, mas, mp, rth, bll, psc, pic, qcf, cuesim, tprom, dprom, ndistr, lp, ldp, dbl, distant,recent,medprom,highprom) %>% summarise(Effect=mean(int), SE=sd(int)/sqrt(length(int)))
+# 	ungroup(simMeans)
+# }
+
+compute_int_means <- function(results){
+	params <- results %>% select(Set:weights, -Iteration, -Condition, -Distractor, -latency) %>% distinct()
+	intMeans <- results %>% select(Set, Iteration, Target, Distractor, latency) %>% spread(Distractor, latency) %>% mutate(Effect1 = Match-Mismatch) %>% group_by(Set, Target) %>% summarise(Effect = mean(Effect1), SE=sd(Effect1, na.rm=TRUE)/sqrt(n())) %>% ungroup()
+	left_join(intMeans, params)
+}
+
+compute_cond_means <- function(results){
+	params <- results %>% select(Set:weights, -Iteration, -latency) %>% distinct()
+	condMeans <- results %>% group_by(Set, Condition, Target, Distractor) %>% summarise(Latency = mean(latency), SE=sd(latency, na.rm=TRUE)/sqrt(n()), Acc=mean(acc), Miss=mean(miss), Fail=mean(fail)) %>% ungroup()
+	left_join(condMeans, params)
+}
+
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+## RUN MODEL
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+#model <- model_4cond
+#iterations <- 2
+
+##
+## PARAMETER MATRIX
+##
+create_param_matrix <- function(model, iterations=1000){
+	print("creating parameter matrix...")
+	#
+	cl <<- (cuesim+1)*100
+	set_prameters()
+	#
+	n_params <- length(parameters);
+	#
+	## The total number of combinations is the product of the number of values
+	## for each parameter
+	n_sets <- prod(unlist(lapply(parameters, length)))
+	n_cond <- length(model$target_match)
+	total <- iterations*n_sets*n_cond
+	#
+	print(paste("Conditions: ",n_cond))
+	print(paste("Combinations: ",n_sets))
+	print(paste("Total runs: ",total))
+	#
+	## Set up matrix of parameter combinations.  Rows are model experiments,
+	## columns are parameters.
+	param_combs <- matrix(nrow=n_sets, ncol=n_params);
+	#
+	cumulative_num_combs <- 1;
+	for (p in 1:n_params) {
+		param_combs[,p] <- rep(parameters[p][[1]], each=cumulative_num_combs, length.out=n_sets);
+		cumulative_num_combs <- cumulative_num_combs * length(parameters[p][[1]]);
+	}
+	#
+	param_matrix <- matrix(data=t(param_combs), nrow=total,ncol=n_params, byrow=TRUE);
+ # 
+	condnames <- 1:length(model$target_match)
+	header <- c(idnames,paramnames,actrnames)
+#
+	id_matrix <- matrix(nrow=total, ncol=length(idnames))
+	actr_matrix <- matrix(nrow=total, ncol=length(actrnames))
+	d <- data.frame(cbind(id_matrix,param_matrix,actr_matrix))
+	colnames(d) <- header
+# d$chunk <- rep(chunknames,total/n_chunks)
+	d$Set <- 1:n_sets
+	d$Condition <- rep(condnames, each=n_sets)
+	d$Target <- rep(model$Target, each=n_sets)
+	d$Distractor <- rep(model$Distractor, each=n_sets)
+	# d$fan1 <- rep(model$target_fan, each=n_sets)
+	# d$fan2 <- rep(model$distractor_fan, each=n_sets)
+	d$match1 <- rep(model$target_match, each=n_sets)
+	d$match2 <- rep(model$distractor_match, each=n_sets)
+	d$Iteration <- rep(1:iterations, each=n_sets*n_cond)
+	d$weights <- model$weights
+	return(d)
+}
+
+
+
+run <- function(d){
+	if(VERBOSE) print("computing base-levels...")
+	# 
+	belowzeros <<- NULL
+	total <- nrow(d)
+	d$times1 <- d$lp
+	d$times2 <- d$ldp
+	d$bl1 <- d$blc
+	d$bl2 <- d$dbl
+	d$cl <- (cuesim+1)*100
+	#
+	d$noise1 <- act_r_noise_n(total, d$ans)
+	d$noise2 <- act_r_noise_n(total, d$ans)
+	d$blact1 <- base_act(prom=d$tprom, times=d$times1, C1=d$pic, C2=d$psc, b=d$bl1,  noise=d$noise1, dec=d$bll)
+	d$blact2 <- base_act(prom=d$dprom, times=d$times2, C1=d$pic, C2=d$psc, b=d$bl2,  noise=d$noise2, dec=d$bll)
+	#
+	## Cue match 
+	match1 <- matrix(unlist(d$match1),nrow=total,ncol=length(d$match1[[1]]),byrow=TRUE)
+	match2 <- matrix(unlist(d$match2),nrow=total,ncol=length(d$match2[[1]]),byrow=TRUE)
+	weights <- matrix(unlist(d$weights),nrow=total,ncol=2,byrow=TRUE)
+	#
+	## Match quality (including cue confusion and scaled by base-level activation)
+	cueconf1 <- match1-((match1-1)*(1+d$cuesim))
+	cueconf1[cueconf1==0] <- 1
+	cueconf2 <- match2-((match2-1)*(1+d$cuesim))
+	cueconf2[cueconf2==0] <- 1
+	if(VERBOSE) print("computing match quality...")
+	Qj1 <- match_quality(match=match1, sim=d$cuesim, blact=d$blact1, tau=d$rth, s=d$ans, q=d$qcf, q2=d$qco)
+	Qj2 <- match_quality(match=match2, sim=d$cuesim, blact=d$blact2, tau=d$rth, s=d$ans, q=d$qcf, q2=d$qco)
+	# 
+	## Probability of item given cue 
+	if(VERBOSE) print("computing probability P(i|j)...")
+	P1j <- Qj1 / (Qj1+(Qj2*d$ndistr) +0.0001)
+	P2j <- Qj2 / (Qj1+(Qj2*d$ndistr) +0.0001)
+	# 
+	## Spreading activation
+	if(VERBOSE) print("computing spreading activation...")
+	Sj1 <- spreading_act(Pij=P1j, S=d$mas)
+  Sj2 <- spreading_act(Pij=P2j, S=d$mas)
+	d$Sji_neg <- rowMeans(ifelse(Sj1 < 0 | Sj2 < 0, T, F))
+	# 
+	## Activations
+	if(VERBOSE) print("computing activations...")
+  d$act1 <- d$blact1 + weight_spreading_act(Sji=Sj1, weights=weights*cueconf1, W=d$ga) + mismatch_penalty(Pij=P1j, P=d$mp) + d$noise1
+  d$act2 <- d$blact2 + weight_spreading_act(Sji=Sj2, weights=weights*cueconf2, W=d$ga) + mismatch_penalty(Pij=P2j, P=d$mp) + d$noise2
+  
+	##
+	## FINAL VALUES
+	##
+	if(VERBOSE) print("computing latencies...")
+	d$activation <- ifelse(d$act1>d$act2, d$act1, d$act2)
+	retrieved <- ifelse(d$act1>d$act2, 1, 2)
+	d$retrieved <- ifelse(d$activation>d$rth, retrieved, 0)
+	d$latency <- latency(d$activation, F=d$lf, f=d$le, tau=d$rth)
+	d$acc <- ifelse(d$retrieved==1, 1, 0)
+	d$miss <- ifelse(d$retrieved==2, 1, 0)
+	d$fail <- ifelse(d$retrieved==0, 1, 0)
+	#
+	if(VERBOSE) print("FINISHED")
+	#
+	return(tbl_df(d))
+}
+
+
+
+
+
+
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+## ACT-R
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+activation <- function(Pij=matrix(c(1,1),1,2,T), prom=0, weights=matrix(c(1,1),1,2,T), times=lp, b=blc, noise=act_r_noise(ans), W=ga, dec=bll, S=mas, P=mp, C1=pic, C2=psc){
+  # base_act <- log(times^(-dec)) + bl
+  baseact <- base_act(prom=prom, times=times, C1=C1, C2=C2, b=b, noise=noise, dec=dec)
+  Sji <- S+log(Pij)
+  Sji <- ifelse(Sji==-Inf | is.na(Sji), 0, Sji)
+  ifelse(Sji < 0, print("!!! WARNING: Sji < 0 !!!"), T)
+  Sji <- ifelse(Sji < 0, 0, Sji)
+  Wkj <- W*weights/rowSums(weights)
+  penalty <- Pij-1
+  Pi <- rowSums(P*penalty)
+  act <- baseact + rowSums(Wkj*Sji) + Pi + noise
+  return(act)
+}
+
+
+spreading_act <- function(Pij=matrix(c(1,1),1,2,T), S=mas){
+	Sji <- S+log(Pij)
+  Sji <- ifelse(Sji==-Inf | is.na(Sji), 0, Sji)
+  ifelse(Sji < 0, print("!!! WARNING: Sji < 0 !!!"), T)
+  belowzeros <<- ifelse(Sji < 0, T, F)
+  Sji <- ifelse(Sji < 0, 0, Sji)
+  Sji
+}
+
+weight_spreading_act <- function(Sji=matrix(c(0,0),1,2,T), weights=matrix(c(1,1),1,2,T), W=ga){
+	Wkj <- W*weights/rowSums(weights)
+	rowSums(Wkj*Sji)
+}
+
+mismatch_penalty <- function(Pij=matrix(c(1,1),1,2,T), P=mp){
+	penalty <- Pij-1
+	# penalty <- ifelse(Pij==0, -1, 0)
+  Pi <- rowSums(P*penalty)
+  Pi
+}
+
+
+base_act <- function(prom=0, times=lp, C1=pic, C2=psc, b=blc, noise=act_r_noise(ans), dec=bll){
+  log(times^(-dec)) + b + (C2*(prom+C1))
+}
+
+
+match_quality <- function(match=matrix(c(1,1),1,2,T), sim=cuesim, blact=0, tau=rth, s=ans, q=qcf, q2=qco){
+	qcorr <- 1/(1+q*exp(-(blact-tau-q2)))
+	conf <- match[,c(2,1)]*(1+sim) 
+	(match+conf) * qcorr
+}
+
+
+latency <- function(A, F=lf, f=le, tau=rth){
+  t <- ifelse(A>=tau, F*exp(-f*A)*1000, F*exp(-f*tau)*1000)
+  round(randomize_time(t))
+}
+
+
+act_r_noise_n <- function(n, s=ans){
+  var <- pi^2/3*s^2
+  rnorm(n, 0, sqrt(var))
+}
+
+act_r_noise <- function(s=ans){
+  var <- pi^2/3*s^2
+  rnorm(1, 0, sqrt(var))
+}
+
+
+## Random component ##
+randomize_time <- function(time, n=rand_time){
+  if(n>0) runif(length(time), time*(n-1)/n, time*(n+1)/n) else time
+}
+
+
+noise_off <- function(){
+  ans <<- 0
+  rand_time <<- 0
+}
+
+noise_on <- function(){
+  ans <<- 0.15
+  rand_time <<- 3
+}
+
+
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+## Standard interference model with 4 conditions
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***###
+model_4cond <- list(
+	target_fan = list(c(1,1), c(1,1), c(1,NA), c(1,NA)),
+	distractor_fan = list(c(NA,1), c(NA,NA), c(NA,1), c(NA,NA)),
+	target_match = list(c(1,1), c(1,1), c(1,0), c(1,0)),
+	distractor_match = list(c(0,1), c(0,0), c(0,1), c(0,0)),
+	Target = c("Match","Match","Mismatch","Mismatch"),
+	Distractor = c("Match","Mismatch","Match","Mismatch"),
+	weights = list(c(strWeight(),semWeight()))
+	)
---
+++ server.R
@@ -0,0 +1,331 @@
+# TODO: show activation of antecedent and distractor
+# TODO: Add effect size in numbers
+# TODO: Explanation
+
+
+***REMOVED***
+library(ggplot2)
+# library(ggthemes)
+library(tidyr)
+library(dplyr)
+library(Hmisc)
+options(dplyr.width = Inf)
+
+# source("helpers.r")
+# experiments <<- NULL
+source("experiment-data.r")
+source("inter-act.r")
+
+
+# Fitted meta-parameters
+fit_distant <<- 1.3
+fit_recent <<- 0.7
+fit_lowprom <<- -1
+fit_medprom <<- 0
+fit_highprom <<- 2.5
+fit_confRec <<- -0.5
+fit_confCN <<- -0.5
+fit_memory <<- 1
+
+# Others
+expname <<- "---"
+useDecay <<- TRUE
+
+# Parameters 
+reset_params()
+VERBOSE <<- FALSE
+iterations <- 5000
+# qcf <<- 1 # when qcf>0, base-level influences match-quality (and thus the fan)
+# psc <<- 1 # when psc>0, prominence influences base-level activation
+# rth <<- -4.5
+# lp <<- fit_distant
+# ldp <<- fit_recent
+cl <- cuesim2cl()
+# lf <<- 0.2
+# bll <<- 0.5
+# ans <<- 0.2
+# mas <<- 2
+# mp <<- 0
+# ndistr <<- 1
+# cuesim <<- -1
+# dprom <<- 0
+
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+
+values <- reactiveValues(p1=1, p2=2, p3=3, studyProperties="")
+
+  observe({
+    if(input$reset){
+      reset_params()
+      qcf <<- 10
+      psc <<- 1
+      rth <<- -4.5
+      lp <<- 1
+      ldp <<- 1
+      cl <- cuesim2cl()
+      useDecay <<- TRUE
+      iterations=5000
+      expname <- "---"
+
+      values$studyProperties <- ""
+
+      # updateSliderInput(session, "qcf", value = qcf)
+      updateSliderInput(session, "cl", value = cuesim2cl())
+      updateSliderInput(session, "cueweighting", value = cueweighting)
+      updateSliderInput(session, "dprom", value = dprom)
+      updateSliderInput(session, "ldp", value = ldp)
+      updateSliderInput(session, "lp", value = lp)
+      updateSliderInput(session, "ndistr", value = ndistr)
+      updateSliderInput(session, "lf", value = lf)
+      updateSliderInput(session, "rth", value = rth)
+      updateSliderInput(session, "ans", value = ans)
+      updateSliderInput(session, "mp", value = mp)
+      updateSliderInput(session, "mas", value = mas)
+      updateSliderInput(session, "ga", value = ga)
+      updateSliderInput(session, "lp", value = lp)
+      updateSliderInput(session, "ldp", value = ldp)
+      updateSliderInput(session, "iterations", value = iterations)
+      updateSelectInput(session, "data", selected = expname)
+      updateSelectInput(session, "setparams", selected = expname)
+      updateRadioButtons(session, "useDecay", selected = useDecay)
+    }
+  })
+
+  observe({
+    if(input$setparams!="---"){
+      reset_params()
+      lf <<- 0.1
+      ans <<- 0.2
+      mas <<- 1.5
+      mp <<- 0.25
+      cuesim <<- -1
+      qcf <<- 10
+      psc <<- 1
+      rth <<- -1.5
+      # lp <<- fit_distant
+      # ldp <<- fit_recent
+      cl <- cuesim2cl()
+      useDecay <<- FALSE
+      iterations=5000
+      #
+      expname <- input$setparams
+      params <- getData(expname)[1,]
+      # Interference type
+      if(params$IntType=="pro") {lp <<- fit_recent; ldp <<- fit_distant}
+      if(params$IntType=="retro") {lp <<- fit_distant; ldp <<- fit_recent}
+      # Prominence
+      if(params$Prominence2=="subj_OR_topic") dprom <<- fit_medprom
+      if(params$Prominence2=="subj_AND_topic") dprom <<- fit_highprom
+      if(params$Prominence2=="other") dprom <<- fit_lowprom
+      # Cue confusion
+      if(params$DepType=="reci") cuesim <<- fit_confRec
+      if(params$DepType=="refl" & params$Lang=="CN") cuesim <<- fit_confCN
+      # Number of distractors
+      if(params$DistrPos=="memory_3x") ndistr <<- 3*fit_memory
+
+      # Study property text
+      values$studyProperties <- paste(
+        params$Lang, " ", params$DepType, ", ", params$VerbType, " 
+", params$IntType, "active, ", params$Cue, " 
+", ndistr, " distr. in ", params$DistrPos,
+        sep="")
+    
+      # updateSliderInput(session, "qcf", value = qcf)
+      updateSliderInput(session, "cl", value = cuesim2cl())
+      updateSliderInput(session, "cueweighting", value = cueweighting)
+      updateSliderInput(session, "dprom", value = dprom)
+      updateSliderInput(session, "ldp", value = ldp)
+      updateSliderInput(session, "lp", value = lp)
+      updateSliderInput(session, "ndistr", value = ndistr)
+      updateSliderInput(session, "lf", value = lf)
+      updateSliderInput(session, "rth", value = rth)
+      updateSliderInput(session, "ans", value = ans)
+      updateSliderInput(session, "mp", value = mp)
+      updateSliderInput(session, "mas", value = mas)
+      updateSliderInput(session, "ga", value = ga)
+      updateSliderInput(session, "lp", value = lp)
+      updateSliderInput(session, "ldp", value = ldp)
+      updateSliderInput(session, "iterations", value = iterations)
+      updateSelectInput(session, "data", selected = expname)
+      updateSelectInput(session, "setparams", selected = expname)
+      updateRadioButtons(session, "useDecay", selected = useDecay)
+    }
+
+  })
+
+  # observe({
+  #   if(input$setext){
+  #     reset_params()
+  #     pc <<- 5
+  #     cuesim <<- -0.45
+  #     lf <<- 0.15
+  #     rth <<- -1.5
+  #     ans <<- 0.15
+  #     mp <<- 1.5
+  #     ndistr <<- 1
+  #   }
+  #   updateSliderInput(session, "pc", value = pc)
+  #   updateSliderInput(session, "cl", value = cuesim2cl())
+  #   updateSliderInput(session, "cueweighting", value = cueweighting)
+  #   updateSliderInput(session, "dbl", value = dbl)
+  #   updateSliderInput(session, "ldp", value = ldp)
+  #   updateSliderInput(session, "lp", value = lp)
+  #   updateSliderInput(session, "nd", value = ndistr)
+  #   updateSliderInput(session, "lf", value = lf)
+  #   updateSliderInput(session, "rth", value = rth)
+  #   updateSliderInput(session, "ans", value = ans)
+  #   updateSliderInput(session, "mp", value = mp)
+  #   updateSliderInput(session, "mas", value = mas)
+  #   updateSliderInput(session, "ga", value = ga)
+  # })
+
+  observe({
+    if(input$intRet) {
+      updateSliderInput(session, "lp", value = fit_distant)
+      updateSliderInput(session, "ldp", value = fit_recent)
+    }
+  #   })
+  # observe({
+    if(input$intPro) {
+      updateSliderInput(session, "lp", value = fit_recent)
+      updateSliderInput(session, "ldp", value = fit_distant)
+    }
+    })
+
+  output$littable = renderDataTable({
+      # lit$i <- NULL
+      # lit$Set <- NULL
+      # lit$Mnum <- NULL
+      # lit$TypeClass <- NULL
+      # lit$Type2 <- NULL
+      # lit$PredOrig <- NULL
+      # lit$PromClass <- NULL
+      # lit$Subject <- NULL
+      experiments %>% select(Id, Publication, DepType, TargetType, Effect, SE, VerbType, Lang, Method, Measure, WMC, Cue, IntType, Prominence=Prominence1)
+    })
+
+  update <- observe({
+    runmodel <- input$runmodel
+    iterations <- input$iterations
+    # qcf <<- input$qcf
+    cuesim <<- cl2cuesim(input$cl)
+    cueweighting <<- input$cueweighting
+    dprom <<- input$dprom
+    ldp <<- input$ldp
+    lp <<- input$lp
+    lf <<- input$lf
+    rth <<- input$rth
+    ans <<- input$ans
+    mp <<- input$mp
+    mas <<- input$mas
+    ga <<- input$ga
+    ndistr <<- input$ndistr
+    useDecay <<- input$useDecay
+    if(!useDecay) bll <<- 0 else bll <<- 0.5
+
+    values$studyProperties <- ""
+
+    sets <- create_param_matrix(model_4cond, iterations=iterations)
+    results <- run(sets)
+    # means_int <- simMeans <- compute_int_means(results)
+
+
+    # Latencies
+    # ---------------------------
+    # meanResults <- select(results, Target, Distractor, latency
+
+    (condPlot <- ggplot(results, aes(Distractor, latency, col=Target, group=Target))
+      + stat_summary(fun.y="mean", geom="line")
+      + stat_summary(fun.data="mean_cl_normal", size=.4, show.legend=FALSE)
+      + xlab("Distractor")
+      + ylab("Latency in ms")
+      # + scale_color_hc()
+      # + theme_bw()
+      + scale_color_brewer(palette="Set1")
+      + theme(legend.position="right",legend.key=element_blank())
+      + ggtitle("Retrieval Latency")
+      )
+
+    values$condPlot <- condPlot
+
+    # Interference effect
+    # ---------------------------
+    intResults <- results %>% select(Set, Iteration, Target, Distractor, latency) %>% spread(Distractor, latency) %>% mutate(Effect = Match-Mismatch, Dataset = "Model")
+
+    # Add data
+    if(input$data!="---"){
+      expname1 <- input$data
+      expdata <- getData(expname1)
+      expdata2 <- expdata%>% select(Target, Effect) %>% mutate(Dataset = "Data")
+      intResults <- bind_rows(intResults, expdata2) %>% mutate(Dataset = factor(Dataset, levels=c("Model","Data")))
+
+      # Study property text
+      params <- expdata[1,]
+      values$studyProperties <- paste(
+        params$Lang, " ", params$DepType, ", ", params$VerbType, " 
+", params$IntType, "active, ", params$Cue, " 
+", ndistr, " distr. in ", params$DistrPos,
+        sep="")
+    }
+
+    # Interference plot
+    dodge <- position_dodge(0.9)
+    (intPlot <- ggplot(intResults, aes(Target, Effect, fill=Dataset))
+      + geom_hline(aes(yintercept=0), colour="gray10")
+      + stat_summary(fun.y="mean", geom="bar", position=dodge)
+      + stat_summary(fun.data="mean_cl_normal", width=0, geom="errorbar", show.legend=FALSE, position=dodge)
+      + theme(legend.title=element_blank())
+      + scale_fill_brewer(palette="Set1")
+      + ylab("Distractor Match - Mismatch (ms)")
+      + ggtitle("Interference Effect")
+      )
+
+    values$intPlot <- intPlot
+
+
+    # Retrieval Accuracy
+    # ---------------------------
+    accResults <- results %>% 
+      gather(Retrieval, Proportion, acc, miss, fail) %>% 
+      select(Target, Distractor, Retrieval, Proportion) %>% 
+      mutate(Retrieval = factor(Retrieval, levels=c("acc","miss","fail"), labels=c("Target","Distractor","Failed")), Target = factor(Target, labels=c("Target-Match", "Target-Mismatch"))) %>% group_by(Target, Distractor, Retrieval) %>% summarise_each(funs("mean"))
+
+    # Accuracy plot
+    dodge <- position_dodge(0.9)
+    (accPlot <- ggplot(accResults, aes(Distractor, Proportion, fill=Retrieval))
+      + geom_hline(aes(yintercept=0), colour="gray10")
+      + geom_bar(stat="identity")
+      + scale_fill_brewer(palette="Set1", direction=-1)
+      + ggtitle("Retrieval Accuracy")
+      + facet_wrap(~Target)
+      )
+
+    values$accPlot <- accPlot
+
+  })
+
+
+
+  output$studyProperties <- renderText({
+    values$studyProperties
+    })
+
+
+  output$plot1 <- renderPlot({
+      values$intPlot
+    })
+
+  output$plot2 <- renderPlot({
+      values$condPlot
+    })
+
+  output$plot3 <- renderPlot({
+    values$accPlot
+  })
+
+
+ 
+  })

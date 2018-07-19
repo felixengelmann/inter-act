b3214b6f36677152f1f472d74eeeb9cd0ec5afe2
Felix Engelmann <xilef@posteo.net>
2018-07-19 19:31:59 +0200

    update data prep

--- load_data.R
+++ load_data.R
@@ -1,19 +1,23 @@
 
 library(tidyr)
 library(dplyr)
+library(readr)
 library(stringr)
 
 
 # ===========================
 # Load experiment data
 # =========================== 
-experiments <- read.csv("MetaAnalysisData.reindexed.csv")
+experiments <- read.csv2("MetaAnalysisData.csv")
 experiments$Publication <- as.character(experiments$Publication)
-experiments$Publication[experiments$Publication == "VanDykeE2LoSyn"] <- "VanDyke07E2LoSyn"
-experiments <- experiments %>% separate(Method_Measure, c("Method", "Measure","WMC"), sep = "_") %>% arrange(ID, TargetType) %>% unite(ID_Pub, ID, Publication, remove = FALSE) %>% mutate(Effect = round(as.numeric(as.character(Effect))), SE = round(SE))
 
 experiments$Prominence2 <- factor(experiments$Prominence2, levels = c("other", "subj_OR_topic", "subj_AND_topic"), labels = c("OTHER", "subject OR topic", "subject AND topic"))
 experiments$DepType <- factor(experiments$DepType, labels = c("SV agreement", "SV nonagreement", "Reciprocals", "Reflexives"))
+
+experiments <- experiments %>% arrange(DepType, Prominence2, IntType, Publication, TargetType)
+
+experiments <- experiments %>% separate(Method_Measure, c("Method", "Measure"), sep = "_") %>% mutate(ID = cumsum(ifelse(Publication != lag(Publication) | is.na(lag(Publication)), 1, 0))) %>% unite(ID_Pub, ID, Publication, remove = FALSE) %>% mutate(Effect = round(as.numeric(as.character(Effect))), SE = round(SE))
+
 
 expnames <- c("---", as.character(unique(experiments$ID_Pub)))
 
@@ -26,7 +30,10 @@ getData <- function(expname, measures = c("FPRT","RT")){
 # ===========================
 # Load simulation params
 # =========================== 
-simfit <- read.csv("simfit-batch-restr.csv")
+simfit <- read.csv("simfit-batch-restr2.csv")
+bestproms <- simfit %>% filter(psc==1) %>% group_by(bll, DepType, Prominence2, dprom) %>% summarise(fit = mean(Fit)) %>% filter(fit==min(fit)) %>% select(-fit) %>% rename(bestprom=dprom) %>% ungroup()
+simfit <- left_join(simfit, bestproms) %>% filter(!(psc==1 & dprom!=bestprom))
+
 
 getParams <- function(expname, decay = 0, measures = c("FPRT","RT")){
   if(expname == expnames[1]) return(NA)
